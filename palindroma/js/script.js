659e16538d1f68c2d36aec97e1d4fcae262f3a3e
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-26 15:25:24 +0200

    Add: giraStringa for palindrome

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,14 +1,22 @@
 
-/* funzione che gira la parola:
 
-function giraStringa (str) {
+const UserWord = prompt("Inserisci una parola!");
+
+// funzione che gira la parola:
+
+function giraStringa (UserWord) {
     let girata = "";
 
-    for (let i = str.lenght - 1; i >= 0; i--) {
-        
-    }
+    for (let i = UserWord.length - 1; i >= 0; i--) {
+        girata += UserWord[i];
 
+    }
+    console.log(girata);
 
 }
+giraStringa(UserWord);
 
   
+
+
+
