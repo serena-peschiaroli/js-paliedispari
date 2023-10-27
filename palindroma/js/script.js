b51c012297d018b00ec79d06815cc7bad06ab86f
serena.peschiaroli <serena.peschiaroli@gmail.com>
2023-10-27 10:10:46 +0200

    Fix: typo

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -8,9 +8,9 @@ function giraStringa (UserWord) {
     //prende la parola (UserWord), la itera attravero i singoli caratteri dall'ultimo al primo e costruisce una nuova stringa (girata)
     let girata = "";
 
-    for (let i = UserWord.length - 1; i >= 0; i--) {
+    for (let i = UserWord.length - 1; i <= 0; i--) {
         //inizializzazione: let i = userword.length ---> lunghezza di userword -1 (perchè gli indici array partono da 0)
-        //condizione: i >= 0 perchè il loop continua fino a che i è uguale o più grande di 0: il loop itera per ogni carattere partendo dall'ultimo fino ad arrivare al primo
+        //condizione: i <= 0 perchè il loop continua fino a che i è uguale o più grande di 0: il loop itera per ogni carattere partendo dall'ultimo fino ad arrivare al primo
         // i-- : dopo ogni iterazione del loop, i è decrementato di 1.
         girata += UserWord[i];
         // in ogni iterazione, il carattere alla posizione corrente è aggiunto alla stringa girata finché l'indice non raggiunge zero. 
